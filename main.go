e27e981d217bd293c1322d49d4bc4fc50105d703
wangbai <wangbai@greencheng.com>
2016-07-18 15:52:50 +0800

    test httpserv and monitor

--- main.go
+++ main.go
@@ -1,11 +1,36 @@
 package main
 
 import (
+	"time"
+
 	"github.com/wangbai/peeper/httpserv"
+	"github.com/wangbai/peeper/monitor"
 )
 
 func main() {
-	//dis := &monitor.Discovery {}
-	//d := httpserv.NewDiscoveryResource(dis)
+	serv1 := monitor.NewService("http",
+		[]monitor.Node{
+			monitor.Node{Address: "baidu.com:80"},
+			monitor.Node{Address: "sohu.com:80"},
+		},
+		3,
+		2*time.Second,
+	)
+
+	serv2 := monitor.NewService("https",
+		[]monitor.Node{
+			monitor.Node{Address: "baidu.com:443"},
+			monitor.Node{Address: "sohu.com:443"},
+		},
+		3,
+		2*time.Second,
+	)
+
+	dis := monitor.NewDiscovery()
+	dis.AddService(serv1)
+	dis.AddService(serv2)
+	dis.Start()
+
+	monitor.RegisterDiscoveryLookupHandler(dis)
 	httpserv.Start(11111)
 }
---
+++ monitor/discovery_lookup_handler.go
@@ -0,0 +1,64 @@
+package monitor
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+    "net/http"
+
+	"github.com/wangbai/peeper/httpserv"
+)
+
+// json decode helper for Node
+type node struct {
+	Address string            `json:"address"`
+	Status  bool              `json:"status"`
+	Attr    map[string]string `json:"attr,omitempty"`
+}
+
+// json helper type for Service
+type service []node
+
+// json helper type for Discovery
+type discovery map[string]service
+
+type discoveryLookupHandler struct {
+	dis *Discovery
+}
+
+func RegisterDiscoveryLookupHandler(d *Discovery) {
+	if d == nil {
+		log.Fatal("Discovery Service should not be empty")
+	}
+
+    dh := &discoveryLookupHandler{dis: d}
+    httpserv.Register("discovery", dh)
+}
+
+func (dh *discoveryLookupHandler) Handle(resp http.ResponseWriter, req *http.Request) {
+    // set header
+    resp.Header().Set("Content-Type", "application/json")
+
+    // compose json data
+	jsonDis := make(map[string]service)
+    ss := dh.dis.GetAllServices();
+    for _, s := range ss {
+        var jsonServ []node
+        for _, n := range s.Nodes {
+            jsonServ = append(jsonServ, node {
+                Address: n.Address,
+                Status: n.Status,
+                Attr: n.Attr,
+            })
+        }
+
+        jsonDis[s.Name] = jsonServ
+    } 
+
+	j, err := json.Marshal(jsonDis)
+	if err != nil {
+		fmt.Fprintf(resp, "%s", err)
+	}
+
+	fmt.Fprintf(resp, "%s", j)
+}
