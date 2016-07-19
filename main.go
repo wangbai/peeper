e134ab0a2226da1425008f5da3a3018a41364a1e
wangbai <wangbai@greencheng.com>
2016-07-19 13:03:17 +0800

    format

--- main.go
+++ main.go
@@ -2,13 +2,13 @@ package main
 
 import (
 	"github.com/wangbai/peeper/config"
-    "github.com/wangbai/peeper/httpserv"
+	"github.com/wangbai/peeper/httpserv"
 	_ "github.com/wangbai/peeper/monitor"
 )
 
 func main() {
-    config.Build("config/");
+	config.Build("config/")
 
-    server := httpserv.NewServer(0)
-    server.Start()
+	server := httpserv.NewServer(0)
+	server.Start()
 }
