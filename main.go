b7553e9b6e1fb3dd9befe5f7ef38fb21aa0a3724
wangbai <wangbai@greencheng.com>
2016-07-20 10:48:55 +0800

    assign config dir from command line

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"flag"
+	"os"
+
 	"github.com/wangbai/peeper/config"
 	"github.com/wangbai/peeper/httpserv"
 	_ "github.com/wangbai/peeper/monitor"
 )
 
+var configDir string
+
 func main() {
-	config.Build("config/")
+	parseCmdLine()
+
+	config.Build(configDir)
 
 	server := httpserv.NewServer()
 	server.Start()
+}
+
+func parseCmdLine() {
+	flag.StringVar(&configDir, "d", "", "directory for config files")
+	flag.Parse()
+
+	if configDir == "" {
+		flag.Usage()
+		os.Exit(1)
+	}
 }
