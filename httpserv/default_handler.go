1f7192899ad8aba4f1bd7159d0537ba66ff2407a
wangbai <wangbai@greencheng.com>
2016-07-21 14:26:10 +0800

    http status change

--- httpserv/default_handler.go
+++ httpserv/default_handler.go
@@ -12,7 +12,8 @@ type defaultHandler struct {
 func (d defaultHandler) Handle(resp http.ResponseWriter, req *http.Request) {
 	// set header
 	resp.Header().Set("Content-Type", "text/plain")
-
+    resp.WriteHeader(http.StatusNotFound)
+    
 	// compose body
 	content := fmt.Sprintf("No handler for Request URI:%s", req.RequestURI)
 
